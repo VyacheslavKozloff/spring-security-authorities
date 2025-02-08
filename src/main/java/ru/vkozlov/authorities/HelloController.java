39699ccb1a4501279a793aca1e3d5306ec1aa617
VyacheslavKozloff <vyacheslavkozlof@gmail.com>
2025-02-08 14:52:12 +0300

    Добавлена конфигурация для inMemory аутентификации. Добавлен entryPoint /hello

---
+++ src/main/java/ru/vkozlov/authorities/HelloController.java
@@ -0,0 +1,15 @@
+package ru.vkozlov.authorities;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "hello";
+	}
+	
+}
