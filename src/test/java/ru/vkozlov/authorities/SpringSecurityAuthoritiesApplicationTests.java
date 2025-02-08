d993b50010098d1a548c9e486c1066705d055e22
VyacheslavKozloff <vyacheslavkozlof@gmail.com>
2025-02-08 13:21:19 +0300

    Проект для работы с авторизацией

---
+++ src/test/java/ru/vkozlov/authorities/SpringSecurityAuthoritiesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vkozlov.authorities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAuthoritiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
